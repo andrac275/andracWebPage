eb52b5b71cc4a6abd21037aec90191cc6d5c4352
Andreu Mut <andrac275@gmail.com>
2024-07-22 22:30:15 +0200

    Tailwind

---
+++ front-office/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: { textOpacity: ["dark"] },
+  },
+  plugins: [],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      blue: {
+        light: "#85d7ff",
+        DEFAULT: "#1fb6ff",
+        dark: "#009eeb",
+      },
+      pink: {
+        light: "#ff7ce5",
+        DEFAULT: "#ff49db",
+        dark: "#ff16d1",
+      },
+    },
+  },
+};

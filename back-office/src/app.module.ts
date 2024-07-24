68ea01a3033a542c5318f37870a155e80737bafb
Andreu Mut <andrac275@gmail.com>
2024-07-24 16:53:14 +0200

    start again with nest 8.2.6

--- back-office/src/app.module.ts
+++ back-office/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { TasksModule } from './tasks/tasks.module';
-import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule, AuthModule, ProjectsModule, UsersModule],
+  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
 })
 export class AppModule {}
